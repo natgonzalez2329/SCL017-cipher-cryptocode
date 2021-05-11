dbf0812387eea45cf390dd792a56c68b7053e610
Natasha Gonzalez <natgonzalez2329@gmail.com>
2021-05-10 23:07:19 -0300

    1er commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const homeTab = document.getElementById("home-tab");
+const aboutTab = document.getElementById("about-tab");
+const contactTab = document.getElementById("contact-tab");
+
+aboutTab.style.display = "none";
+contactTab.style.display = "none";
