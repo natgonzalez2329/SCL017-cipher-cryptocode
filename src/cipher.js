3d7381598cf4d604a5abd5c07f1b87643f67a48c
Natasha Gonzalez <natgonzalez2329@gmail.com>
2021-05-14 00:40:54 -0300

    js de cipher-encrypt y comienzo del cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
 const cipher = {
-  // ...
+  //...
 };
 
 export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Metodo Encode
+/*function codificarEntidad(str) {
+  var array = [];
+  for (var i=str.length-1;i>=0;i--) {
+    array.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+  }
+  return array.join('');
+}*/
+
+
+//Metodo Decode
+/*function descodificarEntidad(str) {
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
+}*/
--- src/index.js
+++ src/index.js
@@ -13,31 +13,87 @@ contactTab.style.display = "none";
 cipherEncrypt.style.display = "block";
 
 const btnhome = document.getElementById("home");
-btnhome.addEventListener("click",home);
-
-const btnabout = document.getElementById("about");
-btnabout.addEventListener("click",about);
-
-const btncontact = document.getElementById("contact");
-btncontact.addEventListener("click",contact);
-
-function home() {
+btnhome.addEventListener("click", () => {
     homeTab.style.display = "block";
     aboutTab.style.display = "none";
     contactTab.style.display = "none";
     cipherEncrypt.style.display = "block";
-}
+});
 
-function about() {
+const btnabout = document.getElementById("about");
+btnabout.addEventListener("click", () => {
     aboutTab.style.display = "block";
     homeTab.style.display = "none";
     contactTab.style.display = "none";
     cipherEncrypt.style.display = "none";
-}
+});
 
-function contact() {
+const btncontact = document.getElementById("contact");
+btncontact.addEventListener("click", () => {
     contactTab.style.display = "block";
     homeTab.style.display = "none";
     aboutTab.style.display = "none";
     cipherEncrypt.style.display = "none";
-}
+});
+ 
+
+/*const btnEncode = document.getElementById("btnEncode");
+btnTest.addEventListener("click",btnEncode);
+
+function btnEncode() {
+    const encryptInputMessage = document.getElementById("encrypt-input_message").value;
+
+    //check console
+    console.log(encryptInputMessage);
+    
+
+    //document.getElementById("cryptogram-input_result").innerHTML= "encrypt";//meter el cifrado
+
+
+}*/
+
+/*const submit = e => {
+    e.preventDefault();
+    cryptogram.innerHTML = "";
+    shiftMessage();//donde se ejecuta el mensaje original
+  }
+
+  encryptInput.onsubmit = submit;*/
+
+
+  //CIPHER
+  const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+      const encryptInputMessage = document.getElementById("encrypt-input_message");
+      const encryptInput = document.getElementById("encrypt-input");
+      const cryptogram = document.getElementById("cryptogram");
+      const cryptogramInputResult = document.getElementById("cryptogram-input_result");
+      const changeInputOffset = document.getElementById("change-input_offset");
+      const btnEncode = document.getElementById("btnEncode")
+
+
+    encryptInputMessage.addEventListener("keyup", () =>{
+        encryptInputMessage.value = encryptInputMessage.value.toUpperCase();
+    });
+    btnEncode.addEventListener("click", () =>{
+        const textMessage = encryptInputMessage.value;
+        const changeOffset = changeInputOffset.value;
+        cryptogramInputResult.value = encodeText(textMessage,changeOffset);
+    });
+
+    /*ADD BUTTON DECODE=>HTML
+    btnDecode.addEventListener("click", () =>{
+        const textMessage = encryptInputMessage.value;
+        const changeOffset = changeInputOffset.value;
+        cryptogramInputResult.value = decodeText(textMessage,changeOffset);
+    });*/
+
+    //function to encode the textMessage by the chengeOffset to get the textResult-this is a new var to put-
+    const encodeText(textMessage,changeOffset){
+        const textResult = "";
+        const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        
+    }
+
+
+
+
