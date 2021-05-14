2edd19a3908fc654bd066704469870fd2986d1bb
Natasha Gonzalez <natgonzalez2329@gmail.com>
2021-05-14 15:10:28 -0300

    comienzo cipher

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const aboutTab = document.getElementById("about-tab");
 const contactTab = document.getElementById("contact-tab");
 const cipherEncrypt = document.getElementById("cipher-encrypt");
 
-homeTab.style.display = "block"
+homeTab.style.display = "block";
 aboutTab.style.display = "none";
 contactTab.style.display = "none";
 cipherEncrypt.style.display = "block";
@@ -37,21 +37,6 @@ btncontact.addEventListener("click", () => {
 });
  
 
-/*const btnEncode = document.getElementById("btnEncode");
-btnTest.addEventListener("click",btnEncode);
-
-function btnEncode() {
-    const encryptInputMessage = document.getElementById("encrypt-input_message").value;
-
-    //check console
-    console.log(encryptInputMessage);
-    
-
-    //document.getElementById("cryptogram-input_result").innerHTML= "encrypt";//meter el cifrado
-
-
-}*/
-
 /*const submit = e => {
     e.preventDefault();
     cryptogram.innerHTML = "";
@@ -68,32 +53,50 @@ function btnEncode() {
       const cryptogram = document.getElementById("cryptogram");
       const cryptogramInputResult = document.getElementById("cryptogram-input_result");
       const changeInputOffset = document.getElementById("change-input_offset");
-      const btnEncode = document.getElementById("btnEncode")
+      const btnEncode = document.getElementById("btnEncode");
+      const btnDecode = document.getElementById("btnDecode");
+      
 
 
     encryptInputMessage.addEventListener("keyup", () =>{
         encryptInputMessage.value = encryptInputMessage.value.toUpperCase();
     });
+
     btnEncode.addEventListener("click", () =>{
         const textMessage = encryptInputMessage.value;
         const changeOffset = changeInputOffset.value;
         cryptogramInputResult.value = encodeText(textMessage,changeOffset);
     });
 
-    /*ADD BUTTON DECODE=>HTML
     btnDecode.addEventListener("click", () =>{
         const textMessage = encryptInputMessage.value;
         const changeOffset = changeInputOffset.value;
         cryptogramInputResult.value = decodeText(textMessage,changeOffset);
-    });*/
+    });
 
     //function to encode the textMessage by the chengeOffset to get the textResult-this is a new var to put-
-    const encodeText(textMessage,changeOffset){
-        const textResult = "";
-        const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-        
+    const encodeText = (textMessage,changeOffset) =>{
+        let textResult = "";
+        const backToAlphabetM;// declarar variable afuera, si no se pone opaca.
+
+        for (let i = 0; i < textMessage.length; i++) {
+            const codeAscii = textMessage[i].charCodeAt();
+            if (codeAscii != alphabet[i].charCodeAt()) {
+                textResult += String.fromCharCode(codeAscii);
+            }
+
+            if(codeAscii >= 65 && codeAscii <= 90) {
+                console.log(codeAscii);
+                backToAlphabetM = (codeAscii - 65 + changeOffset);
+                while(backToAlphabetM < 0) {
+                    backToAlphabetM = (codeAscii - 65 + changeOffset);
+                }
+                let formulaEncode = (backToAlphabetM) % 26 + 65;
+                textResult += String.fromCharCode(formulaEncode);
+                console.log(textResult);
+            }
+        /* if(codeAscii <= 65 && codeAscii >= 90) {
+            textResult += textMessage[i];
+        } */
     }
-
-
-
-
+}
